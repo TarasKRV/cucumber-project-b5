b573e69e59563dbbb7a4ee6faa22e2b5de17de87
TarasKRV <spirit.see@icloud.com>
2025-09-04 21:16:15 -0500

    Day 11

---
+++ src/test/java/io/loop/utilities/GeneralConstants.java
@@ -0,0 +1,19 @@
+package io.loop.utilities;
+
+public class GeneralConstants {
+
+    // browser types
+    public static final String CHROME = "chrome";
+    public static final String FIREFOX = "firefox";
+    public static final String SAFARI = "safari";
+
+    // herokuapp  url
+    public static final String HEROKUAPP_URL = "https://the-internet.herokuapp.com/forgot_password";
+
+    public static final String HEROKUAPP_URL_FPASSWORD = "https://the-internet.herokuapp.com/forgot_password";
+    public static final String HEROKUAPP_URL_DROPDOWN = "http://the-internet.herokuapp.com/dropdown";
+    public static final String NBA_COM = "https://nba.com";
+
+    public static final String EXPECTED_HEADER_DROPDOWN_NAME = "Dropdown List";
+
+}
---
+++ src/test/java/io/loop/utilities/LoopCampConstants.java
@@ -0,0 +1,30 @@
+package io.loop.utilities;
+
+public class LoopCampConstants {
+
+    // url for registration form
+    public static final String REGISTRATION_FORM_URL = "https://loopcamp.vercel.app/registration_form.html";
+    public static final String RADIOBUTTON_URL = "https://loopcamp.vercel.app/radio-buttons.html";
+
+    // messages
+    public static final String EXPECTED_HEADER_FOR_FORM = "Registration form";
+    public static final String EXPECTED_PLACEHOLDER_FOR_FIRSTNAME = "first name";
+
+
+    public static final String LOOPCAMP_FORGOT_PASSWORD_PAGE = "https://loopcamp.vercel.app/forgot-password.html";
+
+    public static final String EXPECTED_HEADER_FORGOT_PASSWORD_LOOPCAMP = "Forgot Password";
+
+
+    public static final String EXPECTED_EMAIL_LOOPCAMP = "E-mail";
+
+
+    public static final String EXPECTED_EMAILINPUT_LOOPCAMP = "email";
+
+    public static final String EXPECTED_RETVIEVEPASSWORD_LOOPCAMP = "Retrieve password";
+
+    public static final String EXPECTED_POWEREDBY_LOOPCAMP = "Powered by LOOPCAMP";
+
+    public static final String RADIO_BUTTON_URL = "https://loopcamp.vercel.app/radio-buttons.html";
+
+}
