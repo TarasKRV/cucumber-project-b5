6aba9beeedf758e8fad2c8ee7ab3cda8fa58c5d3
TarasKRV <spirit.see@icloud.com>
2025-09-14 13:44:03 -0500

    Day 14

--- src/test/java/io/loop/step_definitions/ProductStepDef.java
+++ src/test/java/io/loop/step_definitions/ProductStepDef.java
@@ -23,6 +23,7 @@ public class ProductStepDef {
         Driver.getDriver().get(ConfigurationReader.getProperties("product.url"));
         LOG.info("User on home page");
     }
+
     @Then("User should be able to see expected prices in the following products")
     public void user_should_be_able_to_see_expected_prices_in_the_following_products(List<Map<String, String>> productDetails) {
         for (Map<String, String> productDetail : productDetails) {
@@ -35,9 +36,61 @@ public class ProductStepDef {
             String actualPrice = pages.getProductPage().getProductPrice(productDetail.get("Product"));
             String expectedPrice = productDetail.get("expectedPrice");
             assertEquals("Expected price not match" + expectedPrice, actualPrice, expectedPrice);
-            LOG.info("Validation of the price for " + productDetail.get("Category")+ ", for " + productDetail.get("Product") + " expected price is " + expectedPrice+ " - actual price is " + actualPrice);
+            LOG.info("Validation of the price for " + productDetail.get("Category") + ", for " + productDetail.get("Product") + " expected price is " + expectedPrice + " - actual price is " + actualPrice);
 
         }
+
+    }
+
+    @Then("User should be able to see expected prices in the following products with listOfLists")
+    public void user_should_be_able_to_see_expected_prices_in_the_following_products_with_list_of_lists(List<List<String>> productDetails) {
+        for (List<String> productDetail : productDetails) {
+            pages.getProductPage().clickCategory(productDetail.get(0));
+            String actualPrice = pages.getProductPage().getProductPrice(productDetail.get(1));
+            String expectedPrice = productDetail.get(2);
+            assertEquals("Expected price not match" + expectedPrice, actualPrice, expectedPrice);
+            LOG.info("Validation of the price for " + productDetail.get(0) + ", for " + productDetail.get(1) + " expected price is " + expectedPrice + " - actual price is " + actualPrice);
+        }
+
+    }
+
+    @Then("user should be able to see the names")
+    public void user_should_be_able_to_see_the_names(Map<String, List<String>> student) {
+
+        List<String> group2 = student.get("Group 2");
+
+
+    }
+
+    @Then("User should be able to see expected prices in the following products with map")
+    public void user_should_be_able_to_see_expected_prices_in_the_following_products_with_map(Map<String, List<String>> productsDetails)  {
+
+
+        for (Map.Entry<String, List<String>> entry : productsDetails.entrySet()) {
+            String category = entry.getKey();
+            List<String> products = entry.getValue();
+
+            for (String productPrice : products) {
+
+                try {
+                    String[] arr1 = productPrice.split("-");
+                    String product = arr1[0].trim();
+                    String expectedPrice = arr1[1].trim();
+
+
+                    pages.getProductPage().clickCategory(category);
+
+                    String actualPrice = pages.getProductPage().getProductPrice(product);
+
+                   assertEquals("Expected price not match" + expectedPrice, actualPrice, expectedPrice);
+                    LOG.info("Expected price {} -  Actual price {}", expectedPrice, actualPrice);
+
+
+                } catch (Exception e) {
+                }
+            }
+        }
+
 
     }
 
--- src/test/resources/features/product.feature
+++ src/test/resources/features/product.feature
@@ -2,8 +2,8 @@ Feature: product data table practice
 
 
   @listOfMap
-  Scenario: verify each product price
   # practice List<Map<String, String>>
+  Scenario: verify each product price
     Given User is on the HomePage
     Then User should be able to see expected prices in the following products
       | Category | Product           | expectedPrice |
@@ -32,3 +32,35 @@ Feature: product data table practice
       #  {Category=Monitors, Product=Apple monitor 24, expectedPrice=400}
       #  List Element 8 (Map):
       #  {Category=Monitors, Product=ASUS Full HD, expectedPrice=230}
+
+
+
+  @ListOfLists
+  Scenario: verify each product price ListOfList
+    Given User is on the HomePage
+    Then User should be able to see expected prices in the following products with listOfLists
+      | Phones   | Samsung galaxy s6 | 360 |
+      | Phones   | Nokia lumia 1520  | 820 |
+      | Phones   | Nexus 6           | 650 |
+      | Laptops  | Sony vaio i5      | 790 |
+      | Laptops  | Sony vaio i7      | 790 |
+      | Laptops  | MacBook air       | 700 |
+      | Monitors | Apple monitor 24  | 400 |
+      | Monitors | ASUS Full HD      | 230 |
+
+
+  @mapList
+# Map<String, List<String>>
+  Scenario: verify student names
+    Then user should be able to see the names
+      | Group 2 | nadir | feyruz | Jane   |
+      | Group 3 | vika  | suidum | savlat |
+
+  @mapListProduct
+  Scenario: verify each product price map
+    Given User is on the HomePage
+    Then User should be able to see expected prices in the following products with map
+
+      | Phones   | Samsung galaxy s6    - 360 | Nokia lumia 1520  - 820 | Nexus 6   -  650  |
+      | Laptops  | Sony vaio i5         - 790 | Sony vaio i7      - 790 | MacBook air - 700 |
+      | Monitors | Apple monitor 24     - 400 | ASUS Full HD      - 230 |                   |
